ecb5967aed9a00f8e1023aed6e4637567f7f34ec
Jhon Marlian <Congak48@gmail.com>
2024-08-03 04:16:27 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xFEa0abB32F3187f42A0dE9AE7C0A505eA46672B0'
+export const ProxyAdmin = '0xbA067C96A3D3DeA0dB38e8914bE13836742f5302'
+export const SWTRImplementation = '0x05234d5a5Fc1Ab8C5B28Fd88059ec4d43E84CDEC'
     
